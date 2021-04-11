ae1ff5efa6f4c98eff22634e720808799c9d70df
Norris Co <53540356+norrisco@users.noreply.github.com>
2021-04-11 19:04:55 -0400

    Added login functionality and stripped bearer token from hash

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Components/Login'
+import { getTokenFromUrl } from './Components/Spotify';
 
-function App() {
+const App = () => {
+  const [token, setToken] = useState(null);
+
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    const _token = hash.access_token;
+    window.location.hash = '';
+
+    if (_token) {  
+      setToken(_token)
+    };
+
+    console.log('TOKEN 👉', _token);
+  }, []);
+
   return (
     <div className="App">
 
-      <Login />
+      { token ? ( <h1>LOGGED-IN</h1> ): ( <Login /> ) }
 
+      {/* <Login /> */}
     </div>
   );
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../CSS/Login.css';
+import { loginUrl } from "./Spotify";
 
 const Login = () => {
     return (
         <div className="loginContainer">
             
             {/* Spotify Logo */}
-            <img src={`${process.env.PUBLIC_URL}/assets/spotify_logo_green.png`} alt="Spotify Logo" />
+            <img src={`${process.env.PUBLIC_URL}/assets/spotify_logo_green.png`} alt='Spotify Logo' />
             
             {/* Login with Spotify button */}
-            <a>Login with Spotify</a>
+            <a href={loginUrl}>Login with Spotify</a>
 
         </div>
     );
