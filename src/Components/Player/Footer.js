ba38349c2780601008104d43090ec7a7c68d5e74
Norris Co <53540356+norrisco@users.noreply.github.com>
2021-04-12 22:29:44 -0400

    Added static album cover and song

--- src/Components/Player/Footer.js
+++ src/Components/Player/Footer.js
@@ -1,18 +1,21 @@
 import '../../CSS/Footer.css';
 import { Grid, Slider } from '@material-ui/core';
 
-// import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
-// import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-// import SkipNextIcon from '@material-ui/icons/SkipNext';
-// import ShuffleIcon from '@material-ui/icons/Shuffle';
-// import RepeatIcon from '@material-ui/icons/Repeat';
 import { PlayCircleOutline, SkipPrevious, SkipNext, Shuffle, Repeat, PlaylistPlay, VolumeDown } from '@material-ui/icons';
 
 const Footer = () => {
     return (
         <div className='footerContainer'>
             <div className='footerLeft'>
-                <p>Album and song details</p>
+                <img
+                    className='albumCover'
+                    src='https://upload.wikimedia.org/wikipedia/en/7/79/Owl-city-ocean-eyes-2009.jpg'
+                    alt='Ocean Eyes'
+                />
+                <div className='songInfo'>
+                    <h4>Fireflies</h4>
+                    <p>Owl City</p>
+                </div>
             </div>
 
             <div className='footerCenter'>
