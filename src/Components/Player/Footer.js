3f42091c99309658abcb036ad15cff26c9e17a38
Norris Co <53540356+norrisco@users.noreply.github.com>
2021-04-12 22:24:16 -0400

    Added icons for footerCenter and footerRight

--- src/Components/Player/Footer.js
+++ src/Components/Player/Footer.js
@@ -1,9 +1,41 @@
 import '../../CSS/Footer.css';
+import { Grid, Slider } from '@material-ui/core';
+
+// import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+// import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+// import SkipNextIcon from '@material-ui/icons/SkipNext';
+// import ShuffleIcon from '@material-ui/icons/Shuffle';
+// import RepeatIcon from '@material-ui/icons/Repeat';
+import { PlayCircleOutline, SkipPrevious, SkipNext, Shuffle, Repeat, PlaylistPlay, VolumeDown } from '@material-ui/icons';
 
 const Footer = () => {
     return (
         <div className='footerContainer'>
-            <h1>[Footer]</h1>
+            <div className='footerLeft'>
+                <p>Album and song details</p>
+            </div>
+
+            <div className='footerCenter'>
+                <Shuffle className='footer_green'/>
+                <SkipPrevious className='footer_green'/>
+                <PlayCircleOutline fontSize='large' className='footer_green'/>
+                <SkipNext className='footer_green'/>
+                <Repeat className='footer_green'/>
+            </div>
+
+            <div className='footerRight'>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlay />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDown />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-labelledby='continuous-slider' />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
