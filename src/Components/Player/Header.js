19b30bf76af83aedd8bd09b1aeea74cc3431103a
Norris Co <53540356+norrisco@users.noreply.github.com>
2021-04-13 17:12:35 -0400

    Updated header to be dynamic

--- src/Components/Player/Header.js
+++ src/Components/Player/Header.js
@@ -1,17 +1,29 @@
 import '../../CSS/Header.css';
+import { Avatar } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
+import { useStateValue } from '../../StateProvider';
 
 const Header = () => {
+    const [{ user }, dispatch] = useStateValue();
+
     return (
-        <>
+        <div className='headerContainer'>
             <div className='headerLeft'>
                 <Search />
+                <input 
+                    placeholder='Search for artists and songs'
+                    type='text'
+                />
             </div>
 
             <div className='headerRight'>
-                
+                <Avatar
+                    src={user?.images[0]?.url}
+                    alt={user?.display_name}
+                />
+                <h4>{user?.display_name}</h4>
             </div>
-        </>
+        </div>
     )
 }
 
